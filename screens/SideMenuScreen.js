20b8dfec0e441a4e8d5411c0d6380350bc3bbc38
IQlikeinBREAD <filaret2003@gmail.com>
2024-03-25 18:34:58 +0100

    Added user avatar and name. Later delete user.png.

--- screens/SideMenuScreen.js
+++ screens/SideMenuScreen.js
@@ -1,26 +1,39 @@
 import "react-native-gesture-handler";
 import React from 'react';
-import { View, StyleSheet, Settings } from "react-native";
-import {
-    FontAwesome5,
-    FontAwesome6,
-    Ionicons,
-    MaterialIcons
-} from "@expo/vector-icons";
+import { View, StyleSheet, Settings,Image,Text } from "react-native";
+import {FontAwesome5,FontAwesome6,Ionicons,MaterialIcons} from "@expo/vector-icons";
 import { NavigationContainer } from '@react-navigation/native';
-import { createDrawerNavigator } from "@react-navigation/drawer";
+import {DrawerItemList , createDrawerNavigator } from "@react-navigation/drawer";
 import CameraScreen from "./CameraScreen";
 import FriendsScreen from "./FriendsScreen";
 import GroupsScreen from "./GroupsScreen";
 import ReceiptScreen from "./ReceiptScreen";
 import SettingsScreen from "./SettingsScreen";
-
+import { SafeAreaView } from "react-native-safe-area-context";
+import user from "../assets/user.png"
 const Drawer = createDrawerNavigator();
+
 
 function SideMenuScreen() {
     return (
          <NavigationContainer>
-            <Drawer.Navigator screenOptions={styles}>
+            <Drawer.Navigator 
+            
+            drawerContent={
+                (props) => {
+                    return(
+                        <SafeAreaView>
+                            <View style={userAvatarBackGroundStyle}>
+                                <Image source={user} style={userAvatarStyle}/>
+                                <Text>Adam Mickiewicz</Text>
+                            </View>
+                            <DrawerItemList {...props}/>
+                        </SafeAreaView>
+                    )
+                }
+            }
+
+            screenOptions={globalNavBarStyle}>
                 <Drawer.Screen
                     name="Camera"
                     options={
@@ -100,7 +113,7 @@ function SideMenuScreen() {
 
 export default SideMenuScreen;
 
-const styles = StyleSheet.create({
+const globalNavBarStyle = StyleSheet.create({
     drawerStyle:{
         backgroundColor : "#fff",
         width :250
@@ -117,3 +130,21 @@ const styles = StyleSheet.create({
         color:"#111"
     }
 });
+
+const userAvatarBackGroundStyle = StyleSheet.create({
+    height:200,
+    width: '100%',
+    justifyContent:"center",
+    alignItems:"center",
+    borderBottomColor:"#f4f4f4",
+    borderBottomWidth: 1,
+    backgroundColor: "gray"
+    }
+);
+
+const userAvatarStyle = StyleSheet.create({
+    height:130,
+    width:130,
+    borderRadius:65
+}
+);
