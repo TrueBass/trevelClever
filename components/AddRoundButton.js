6125a821c9c8c19ca321446be397f198449141aa
IQlikeinBREAD <filaret2003@gmail.com>
2024-04-13 18:25:50 +0200

    Changed GroupsScreen. Added in components new button(AddRoundButton)

---
+++ components/AddRoundButton.js
@@ -0,0 +1,33 @@
+
+import { View, Text, Pressable, StyleSheet } from 'react-native';
+
+function AddRoundButton({onPress}){
+  return (
+    <View style={styles.roundButton}>
+      <Pressable style={({pressed}) => pressed?[styles.roundButton,styles.pressed]:styles.roundButton } onPress={onPress}>
+        <Text style={styles.plusIcon}>+</Text>
+      </Pressable>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  roundButton: {
+    width: 70,
+    height: 70,
+    borderRadius: 70,
+    backgroundColor: '#a362cfff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  plusIcon: {
+    fontSize: 45,
+    color: 'white',
+    justifyContent: 'center'
+  },
+  pressed: {
+    opacity: 0.75,
+}
+});
+
+export default AddRoundButton;
--- screens/GroupsScreen.js
+++ screens/GroupsScreen.js
@@ -1,11 +1,38 @@
-import {View,Text} from "react-native"
+import { React, useState } from "react";
+import {View,Text,StyleSheet,FlatList,Button} from "react-native";
+import AddRoundButton from '../components/AddRoundButton';
 
 function GroupsScreen(){
     return(
-        <View>
-            <Text>Testowyj ekran</Text>
+        <View style={styles.container}>
+            <View style={styles.groupListContainer}>
+                <Text>Gfgfgfg</Text>
+            </View>
+            <View style={styles.buttonContainer}>
+                <AddRoundButton/>
+            </View>
         </View>
     );
 }
 
 export default GroupsScreen;
+
+const styles = StyleSheet.create({
+container: {
+        flex: 1,
+        justifyContent: 'center',
+},
+groupListContainer:{
+    flex:5,
+    borderWidth:1,
+    borderColor: 'red'
+},
+buttonContainer:{
+    flex:1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderWidth:1,
+    borderColor: 'red'
+}
+
+});
